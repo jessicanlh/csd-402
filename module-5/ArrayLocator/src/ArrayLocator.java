ac75c53db1e28876b9a44421ede9069e35f683e8
jessicanlh <jessicanlh@gmail.com>
2025-02-02 19:31:55 -0600

    modules 5 and 6

--- module-5/ArrayLocator/src/ArrayLocator.java
+++ module-5/ArrayLocator/src/ArrayLocator.java
@@ -1,4 +1,97 @@
-package PACKAGE_NAME;
+//Jessica Long-Heinicke Module 5 2/2/25
 
 public class ArrayLocator {
+
+    // Method to locate the largest element in a 2D double array
+    public static int[] locateLargest(double[][] arrayParam) {
+        int[] location = {0, 0};
+        double max = arrayParam[0][0];
+
+        for (int i = 0; i < arrayParam.length; i++) {
+            for (int j = 0; j < arrayParam[i].length; j++) {
+                if (arrayParam[i][j] > max) {
+                    max = arrayParam[i][j];
+                    location[0] = i;
+                    location[1] = j;
+                }
+            }
+        }
+        return location;
+    }
+
+    // Method to locate the largest element in a 2D int array
+    public static int[] locateLargest(int[][] arrayParam) {
+        int[] location = {0, 0};
+        int max = arrayParam[0][0];
+
+        for (int i = 0; i < arrayParam.length; i++) {
+            for (int j = 0; j < arrayParam[i].length; j++) {
+                if (arrayParam[i][j] > max) {
+                    max = arrayParam[i][j];
+                    location[0] = i;
+                    location[1] = j;
+                }
+            }
+        }
+        return location;
+    }
+
+    // Method to locate the smallest element in a 2D double array
+    public static int[] locateSmallest(double[][] arrayParam) {
+        int[] location = {0, 0};
+        double min = arrayParam[0][0];
+
+        for (int i = 0; i < arrayParam.length; i++) {
+            for (int j = 0; j < arrayParam[i].length; j++) {
+                if (arrayParam[i][j] < min) {
+                    min = arrayParam[i][j];
+                    location[0] = i;
+                    location[1] = j;
+                }
+            }
+        }
+        return location;
+    }
+
+    // Method to locate the smallest element in a 2D int array
+    public static int[] locateSmallest(int[][] arrayParam) {
+        int[] location = {0, 0};
+        int min = arrayParam[0][0];
+
+        for (int i = 0; i < arrayParam.length; i++) {
+            for (int j = 0; j < arrayParam[i].length; j++) {
+                if (arrayParam[i][j] < min) {
+                    min = arrayParam[i][j];
+                    location[0] = i;
+                    location[1] = j;
+                }
+            }
+        }
+        return location;
+    }
+
+    // Main method to test the above methods
+    public static void main(String[] args) {
+        // Test with a double array
+        double[][] doubleArray = {
+                {1.2, 3.4, 5.6},
+                {7.8, 9.0, 2.3},
+                {4.5, 6.7, 8.9}
+        };
+        int[] largestDouble = locateLargest(doubleArray);
+        int[] smallestDouble = locateSmallest(doubleArray);
+        System.out.println("Largest in double array is at: [" + largestDouble[0] + ", " + largestDouble[1] + "]");
+        System.out.println("Smallest in double array is at: [" + smallestDouble[0] + ", " + smallestDouble[1] + "]");
+
+        // Test with an int array
+        int[][] intArray = {
+                {1, 3, 5},
+                {7, 9, 2},
+                {4, 6, 8}
+        };
+        int[] largestInt = locateLargest(intArray);
+        int[] smallestInt = locateSmallest(intArray);
+        System.out.println("Largest in int array is at: [" + largestInt[0] + ", " + largestInt[1] + "]");
+        System.out.println("Smallest in int array is at: [" + smallestInt[0] + ", " + smallestInt[1] + "]");
+    }
 }
--- module-6/fan/src/FanTest.java
+++ module-6/fan/src/FanTest.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+//Jessica Long-Heinicke Module 6 2/2/25
 
-public class FanTest {
+ class FanTest {
+    public static void main(String[] args) {
+        // Create a fan using the default constructor
+        Fan fan1 = new Fan();
+        System.out.println("Fan 1 (Default Constructor):");
+        System.out.println(fan1.toString());
+
+        // Create a fan using the argument constructor
+        Fan fan2 = new Fan(Fan.FAST, true, 8.5, "blue");
+        System.out.println("\nFan 2 (Argument Constructor):");
+        System.out.println(fan2.toString());
+
+        // Test setter methods on fan1
+        fan1.setOn(true);
+        fan1.setSpeed(Fan.MEDIUM);
+        fan1.setColor("red");
+        fan1.setRadius(5.0);
+
+        // Display updated state of fan1
+        System.out.println("\nFan 1 (After Updating Fields):");
+        System.out.println(fan1.toString());
+
+        // Test getter methods on fan2
+        System.out.println("\nFan 2 Details:");
+        System.out.println("Speed: " + fan2.getSpeed());
+        System.out.println("On: " + fan2.isOn());
+        System.out.println("Radius: " + fan2.getRadius());
+        System.out.println("Color: " + fan2.getColor());
+    }
 }
